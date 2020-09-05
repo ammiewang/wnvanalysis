a6fcfc70da30f77665363ea845908ad252feaac0
Ammie Wang <ammie.wang8@gmail.com>
2020-09-05 00:59:37 -0700

    changed comments

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # wnvanalysis
 These programs were used to analyze West Nile Virus effective population/sequence divergence in the Cohan Lab paper "Delving Below the Species Level To Characterize the Ecological Diversity within the Global Virome: An Exploration of West Nile Virus".
 
-Requirements
+# Requirements
 - python3
 - BioPython
 - ete3
 
-Recommended
+# Recommended
 - pypy3
 
-Steps:
-0. Run ES2.
-1. Run draw_lines.py on the log file from ES2.
-2. Run ecotype_files.py on the outputs from draw_lines.py.
-3. Run pd_overall.py or pd_overall_with_seqdiv.py on the outputs from ecotype_files.py.
+# Steps:
+  0. Run ES2.
+  1. Run draw_lines.py on the log file from ES2.
+  2. Run ecotype_files.py on the outputs from draw_lines.py.
+  3. Run pd_overall.py or pd_overall_with_seqdiv.py on the outputs from ecotype_files.py.
--- draw_lines.py
+++ draw_lines.py
@@ -11,14 +11,16 @@ import logparser
 from ecotypes import Ecotypes
 
 def set_ecs(ecs, t):
+    """creates pairs of (ecotype, common ancestor of ecotype)"""
     s = []
     i = 0
-    while i < len(ecs): #pairs of (ecotype, common ancestor of ecotype)
+    while i < len(ecs):
         s.append((ecs[i], t.get_common_ancestor(ecs[i])),)
         i += 1
     return s
 
-def get_names(t): #get the IDs of all sequences in a tree
+def get_names(t):
+    """get the IDs of all sequences in a tree"""
     new_node = []
     if not t.is_leaf():
         for clade in t.traverse():
@@ -29,7 +31,8 @@ def get_names(t): #get the IDs of all sequences in a tree
     return new_node
 
 
-def back(fas, nwk, ys, t): #outputs less ecotypes than the original, moves backwards on tree
+def back(fas, nwk, ys, t):
+    """outputs less ecotypes than the original, moves backwards on tree"""
     j = len(ys)
     num_ecotypes = int(input("number of desired ecotypes: "))
     while j > num_ecotypes:
@@ -44,23 +47,24 @@ def back(fas, nwk, ys, t): #outputs less ecotypes than the original, moves backw
                 if (set(p_names),p,dist) not in temp:
                     temp.append((set(p_names),p,dist),) #add the merged ecotype to temporary list
             k += 1
-        Keymax = max(temp, key= lambda x: x[2]) #find the putative ecotype in temp whith the greatest distance from the root
+        keymax = max(temp, key= lambda x: x[2]) #find the putative ecotype in temp whith the greatest distance from the root
         #(i.e. the most newly divergent putative ecotype), we will take this as a new ecotype and repeat the above process until num_ecotypes is reached
         to_remove = []
         m = 0
         while m < len(ys): #find and remove any child nodes of the new ecotype from the ecotype list
-            if set(ys[m][0]).issubset(Keymax[0]):
+            if set(ys[m][0]).issubset(keymax[0]):
                 to_remove.append(ys[m])
             m += 1
         for r in to_remove:
             ys.remove(r)
-        ys.append((list(Keymax[0]),Keymax[1]),) #removed ecotypes are merged and appended to the ecotype list
+        ys.append((list(keymax[0]),keymax[1]),) #removed ecotypes are merged and appended to the ecotype list
         j = len(ys)
     less_ecotypes = Ecotypes(fas, nwk)
     less_ecotypes.ecotypes = [y[0] for y in ys]
     return less_ecotypes
 
 def fwd(fas, nwk, ys, t):
+    """outputs more ecotypes than the original, moves forwards on tree"""
     j = len(ys)
     zs = []
     k = 0
@@ -73,8 +77,8 @@ def fwd(fas, nwk, ys, t):
         for z in zs:
             if len(z[1].children) >= 2 and len(z[0]) > 1: #find all nodes/ecotypes that can be separated into child nodes/ecotypes
                 xs.append(z)
-        Keymin = min(xs, key= lambda x: x[2]) #find the node closest to the root and split it
-        ch = Keymin[1].children
+        keymin = min(xs, key= lambda x: x[2]) #find the node closest to the root and split it
+        ch = keymin[1].children
         to_remove = []
         for x in xs:
             for c in ch:
